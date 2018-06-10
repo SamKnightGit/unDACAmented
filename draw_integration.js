7d65fd6e3402bd440d56e9f1c5ea94f8b80f85e2
vienvan <vienvonvan@gmail.com>
2018-06-09 17:34:36 -0700

    employment data, skeleton for 2nd slide

---
+++ draw_integration.js
@@ -0,0 +1,25 @@
+/* global d3 */
+var width=1800;
+var height=600;
+
+function draw_employment() {
+	console.log("employed!");
+}
+
+function draw_education() {
+	console.log("educated!");
+}
+
+function draw_integration() {
+		d3.select("#visuals").remove();
+		d3.select("svg").remove();
+		d3.select(".timeline").append("div")
+			.attr("id", "visuals")
+			.transition().duration(200);
+
+		var svg_canvas = d3.select("#visuals").append("svg")
+			.attr("width", width)
+			.attr("height", height - 100)
+			.attr("class", "integration_canvas")
+			.style("background", "red");
+}
